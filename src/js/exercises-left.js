2909d9e710145a2735c875fea726bccdf69a3d08
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2023-10-22 22:15:11 +0300

    Fix my .html

--- src/js/exercises-left.js
+++ src/js/exercises-left.js
@@ -1,5 +1,4 @@
-// import axios from 'axios';
-// import api from './api.js'
+import api from './api.js'
 
 const refs = {
     quoteText: document.querySelector('.js-quoteText'),
@@ -20,60 +19,31 @@ function chekDate() {
     if (localStorage.getItem('LS-curentDate') !== getDate()) {
         localStorage.setItem("LS-curentDate", getDate());
         // alert("Date is not actual!")y
-        return;
     }
+    api.getQuote().then((data) => {
+        refs.quoteText.textContent = data.quote;
+        refs.quoteAutor.textContent = data.author;
+    }
+    )
 }
+chekDate();
 
-console.log(chekDate());
-// console.log(api.quote());
-console.log(serviceQoute());
-// if (typeof localStorage.getItem('LS-curentDate') == "undefined" || localStorage.getItem('LS-curentDate') === null) {} ;
+//? ***************** fetch api ********************************
 
-// const getQuote = async () => {
-//     try {
-//         const response = await axios.get(`https://your-energy.b.goit.study/api/quote`)
-//         return response.data
-
-//     } catch (err) {
-//         console.error(err.toJSON())
-//     }
-//     console.log(data);
+// function serviceQoute() {
+//     return fetch("https://your-energy.b.goit.study/api/quote")
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw new Error(`Sorry, error status: ${response.status}`)
+//             }
+//             return response.json();
+//         })
+//         .then((data) => {
+//             refs.quoteText.innerHTML = data.quote;
+//             refs.quoteAutor.innerHTML = data.author;
+//         }
+//         )
+//         .catch(error => refs.quoteText.innerHTML = "Sorry. There is currently a problem with the server. Try again a little later. :("); // add to call function
 // }
-// console.log(getQuote());
 
-//? *************************************************
 
-function serviceQoute() {
-    return fetch("https://your-energy.b.goit.study/api/quote")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Sorry, error status: ${response.status}`)
-            }
-            return response.json();
-        })
-        .then((data) => {
-            // console.log(data.author)
-            // console.log(data.quote)
-            refs.quoteText.innerHTML = data.quote;
-            refs.quoteAutor.innerHTML = data.author;
-        }
-        )
-    // .catch(error => alert(" Error http!!! ")); // add to call function
-        .catch(error => refs.quoteText.innerHTML = "Sorry. There is currently a problem with the server. Try again a little later. :("); // add to call function
-}
-//*** 
-
-// refs.quoteText.innerHTML = "data.quote";
-
-// function createMarkup({ author, quote, }) {
-//     return `
-//        <p class="js-quoteText">
-//           <b>Quote: </b>${quote}
-//         </p>
-   
-//        <p class="js-quoteAutor">
-//           <b>Author: </b>${author}
-//         </p>
-//      `;
-// }
-// console.log();
