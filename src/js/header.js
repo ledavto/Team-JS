eb162d970efe9a273f4f868c72618b7d777994ba
Soroka0105 <soroka0105@gmail.com>
2023-10-22 05:58:44 -0400

    fourth commit header

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,7 @@
 const homepage = document.querySelector('.homepageitem');
-// const favoritesLink = document.querySelector('.favoritelink');
 const favoritespageitem = document.querySelector('.favoritespageitem');
+
+// Mobile menu function
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
@@ -30,13 +31,24 @@ const favoritespageitem = document.querySelector('.favoritespageitem');
   });
 })();
 
+// changing style for navigation header
 const changeTextColor = () => {
-  // Event.preventDefault();
   homepage.style.backgroundColor = '#242424';
   homepage.style.color = '#F4F4F4';
-
+  homepage.style.padding = '0';
+  favoritespageitem.style.padding = '0';
   favoritespageitem.style.backgroundColor = '#F4F4F4';
   favoritespageitem.style.color = '#242424';
   favoritespageitem.style.width = '87px';
 };
+
+const changeTextColorHome = () => {
+  homepage.style.backgroundColor = '#F4F4F4';
+  homepage.style.color = '#242424';
+
+  favoritespageitem.style.backgroundColor = '#242424';
+  favoritespageitem.style.color = '#F4F4F4';
+};
+
+homepage.addEventListener('click', changeTextColorHome);
 favoritespageitem.addEventListener('click', changeTextColor);
