0ddeffc37e59dc7d54ccc697e732e64a9bb7437d
ledavto <ser_ega@ukr.net>
2023-10-23 21:20:20 +0300

    add button to top

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './js/header';
 import './js/mobile-menu';
 import './js/hero';
 import './js/exercises';
+import './js/favorites';
 import './js/footer';
 import './js/modal';
 import './js/modal-rating';
+import './js/go-top';
