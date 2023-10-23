272734a29208a4df2f308db28d42399d5d2b0b3c
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2023-10-23 14:33:09 +0300

    CSS-var1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './js/header';
 import './js/mobile-menu';
 import './js/hero';
 import './js/exercises';
+import './js/exercises-left';
 import './js/footer';
 import './js/modal';
 import './js/modal-rating';
