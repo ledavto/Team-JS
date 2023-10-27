fdd70033b1aa1768d4723b47ee7b6d84db6de065
ledavto <ser_ega@ukr.net>
2023-10-27 12:52:36 +0300

    add change button modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -15,12 +15,12 @@ const closeModalTemplate = (closeButtonSelector, closeFunction) => {
 };
 // TODO: Remove event listeners!
 
-const closeExerciseModal = () => {
-  document.querySelector('body').classList.remove('modal-exercise-open');
-};
-
 const openExerciseModal = () => {
   document.querySelector('body').classList.add('modal-exercise-open');
+};
+
+const closeExerciseModal = () => {
+  document.querySelector('body').classList.remove('modal-exercise-open');
 };
 
 const setDataExerciseModal = async id => {
@@ -48,11 +48,6 @@ const setDataExerciseModal = async id => {
 
   openExerciseModal();
 
-  //Закриття по ESCAPE
-  document.addEventListener('keydown', event => {
-    if (event.key === 'Escape') handleClose();
-  });
-
   //Зчитування з LocalStorage
   let checkoutFavorites = JSON.parse(localStorage.getItem('checkout')) ?? [];
 
@@ -61,55 +56,112 @@ const setDataExerciseModal = async id => {
     getListFavor = [...JSON.parse(localStorage.getItem('checkout'))];
   }
 
-  console.log(checkoutFavorites);
-
   //Перевірка наявності у LocalStorage
   const findInLocal = checkoutFavorites.findIndex(
     ({ _id }) => _id === response._id
   );
-  const getElemAdd = document.querySelector('.modal-add-favorates-btn');
-  const getElemRem = document.querySelector('.modal-rem-favorates-btn');
+
+  const getBtnCont = document.querySelector('.modal-button');
+  if (getBtnCont.querySelector('.modal-add-favorates-btn'))
+    getBtnCont.querySelector('.modal-add-favorates-btn').remove();
+  if (getBtnCont.querySelector('.modal-rem-favorates-btn'))
+    getBtnCont.querySelector('.modal-rem-favorates-btn').remove();
 
   if (findInLocal !== -1) {
-    getElemAdd.classList.add('is-hidden');
-    getElemRem.classList.remove('is-hidden');
+    //getElemAdd.classList.add('is-hidden');
+    //console.log(getBtnCont);
+    getBtnCont.insertAdjacentHTML(
+      'afterbegin',
+      `<button class="modal-rem-favorates-btn">
+        <span>Remove favorites</span>
+        <svg class="icon-heart">
+          <use href="./img/icons.svg#icon-heart"></use>
+        </svg>
+      </button>`
+    );
+    //getElemRem.classList.remove('is-hidden');
   } else {
-    getElemAdd.classList.remove('is-hidden');
-    getElemRem.classList.add('is-hidden');
+    //getElemAdd.classList.remove('is-hidden');
+    console.log(getBtnCont);
+    getBtnCont.insertAdjacentHTML(
+      'afterbegin',
+      `<button class="modal-add-favorates-btn">
+        <span>Add favorites</span>
+        <svg class="icon-heart">
+          <use href="./img/icons.svg#icon-heart"></use>
+        </svg>
+      </button>`
+    );
+    //getElemRem.classList.add('is-hidden');
+  }
+
+  function handleRemFav() {
+    getBtnCont.querySelector('.modal-rem-favorates-btn').remove();
+
+    getBtnCont.insertAdjacentHTML(
+      'afterbegin',
+      `<button class="modal-add-favorates-btn">
+        <span>Add favorites</span>
+        <svg class="icon-heart">
+          <use href="./img/icons.svg#icon-heart"></use>
+        </svg>
+      </button>`
+    );
+
+    getBtnCont
+      .querySelector('.modal-add-favorates-btn')
+      .addEventListener('click', handleAddFav);
+
+    //getElemAdd.classList.remove('is-hidden');
+    //getElemRem.classList.add('is-hidden');
+
+    const currentCard = checkoutFavorites.find(
+      ({ _id }) => _id === response._id
+    );
+    const newCardIdx = checkoutFavorites.findIndex(
+      ({ _id }) => _id === response._id
+    );
+
+    if (newCardIdx !== -1) {
+      // якщо це вправа є в localStorage
+      localStorage.clear();
+      getListFavor.splice(newCardIdx, 1);
+      localStorage.setItem('checkout', JSON.stringify(getListFavor));
+    }
+  }
+
+  function handleAddFav() {
+    getListFavor.push(response);
+    localStorage.setItem('checkout', JSON.stringify(getListFavor));
+
+    getBtnCont.querySelector('.modal-add-favorates-btn').remove();
+    getBtnCont.insertAdjacentHTML(
+      'afterbegin',
+      `<button class="modal-rem-favorates-btn">
+        <span>Remove favorites</span>
+        <svg class="icon-heart">
+          <use href="./img/icons.svg#icon-heart"></use>
+        </svg>
+      </button>`
+    );
+    getBtnCont
+      .querySelector('.modal-rem-favorates-btn')
+      .addEventListener('click', handleRemFav);
+
+    // getElemAdd.classList.add('is-hidden');
+    // getElemRem.classList.remove('is-hidden');
   }
 
   if (document.querySelector('.modal-add-favorates-btn')) {
     //Слухач кнопки AddToFavorites
+    const getElemAdd = document.querySelector('.modal-add-favorates-btn');
     getElemAdd.addEventListener('click', handleAddFav);
-    function handleAddFav() {
-      getListFavor.push(response);
-      localStorage.setItem('checkout', JSON.stringify(getListFavor));
-      getElemAdd.classList.add('is-hidden');
-      getElemRem.classList.remove('is-hidden');
-    }
   }
 
   //Слухач кнопки RemoveFavorites
   if (document.querySelector('.modal-rem-favorates-btn')) {
+    const getElemRem = document.querySelector('.modal-rem-favorates-btn');
     getElemRem.addEventListener('click', handleRemFav);
-    function handleRemFav() {
-      getElemAdd.classList.remove('is-hidden');
-      getElemRem.classList.add('is-hidden');
-
-      const currentCard = checkoutFavorites.find(
-        ({ _id }) => _id === response._id
-      );
-      const newCardIdx = checkoutFavorites.findIndex(
-        ({ _id }) => _id === response._id
-      );
-
-      if (newCardIdx !== -1) {
-        // якщо це вправа є в localStorage
-        localStorage.clear();
-        getListFavor.splice(newCardIdx, 1);
-        localStorage.setItem('checkout', JSON.stringify(getListFavor));
-      }
-    }
   }
 };
 
